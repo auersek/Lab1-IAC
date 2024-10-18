f7fd1389e3f6408d6cfb096e774164cda9eb89dc
auersek <122909164+auersek@users.noreply.github.com>
2024-10-18 13:14:14 +0100

    lab 1 task 4 commit

--- task2/counter.sv
+++ task2/counter.sv
@@ -10,8 +10,9 @@ module counter #(
     
 always_ff @(posedge clk, posedge rst) 
     if (rst) count <= {WIDTH{1'b0}};
-    else     count <= count + {{WIDTH-1{1'b0}}, en};
+    else if(en)     count <= count + {{WIDTH-1{1'b0}}, en};
+    else if(~en)     count <= count - {{WIDTH-1{1'b0}}, ~en};
 
 endmodule
 
-/* /dev/tty.usbserial-14410  */
+
---
+++ task4/BCD.sv
@@ -0,0 +1,12 @@
+module bin2bcd #(
+  parameter WIDTH = 8
+)(
+  input logic   x,
+  output logic  bcd
+);
+
+always_ff @ (posedge clk)
+
+
+
+endmodule
